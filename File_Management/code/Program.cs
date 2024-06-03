0356f1643ad465f22de5b2fcea6b68bbacd0dff0
MinmusLin <minmuslin@outlook.com>
2024-06-03 18:41:48 +0800

    Create Program.cs

---
+++ File_Management/code/Program.cs
@@ -0,0 +1,13 @@
+namespace File_Management;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Window.MainWindow());
+    }
+}
